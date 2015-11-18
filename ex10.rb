a38524e0dc7b613486dd6fd8454fc36897066699
valentincrasnier <crasnier.val@gmail.com>
2015-11-18 16:53:07 +0100

    Fini les exercise 1 à 13

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "Im split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* captnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
